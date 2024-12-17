0c619b7fa5446f8c97557fe03dbfa68497f890c7
71200675sabrina <79124326+71200675sabrina@users.noreply.github.com>
2024-12-17 14:11:40 +0700

    Add files via upload

---
+++ GithubApp/app/src/main/java/com/example/githubapp/detail/Injection.kt
@@ -0,0 +1,17 @@
+package com.example.githubapp.detail
+
+import android.content.Context
+import com.example.githubapp.api.ApiConfig
+import com.example.githubapp.database.UserRoomDatabase
+import com.example.githubapp.repository.UserRepository
+
+
+object Injection {
+
+    fun provideRepository(context: Context): UserRepository{
+        val apiService = ApiConfig.getApiService()
+        val database = UserRoomDatabase.getInstance(context)
+        val dao = database.userDao()
+        return UserRepository.getInstance(apiService, dao)
+    }
+}
